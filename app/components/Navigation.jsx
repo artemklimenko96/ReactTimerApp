5cebdb026bcb60ff1be18cbff59796b7a9e5820f
Artem Klimenko <artemklimenko96@gmail.com>
2017-03-19 14:44:04 +0200

    initial commit of timer app

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,41 @@
+var React = require("react");
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = React.createClass({
+  onSearch:function(e){
+    e.preventDefault();
+var location = this.refs.searchRef.value;
+var encodedLocation = encodeURIComponent(location);
+if(location.length>0){
+  this.refs.searchRef.value = "";
+  window.location.hash = '#/?location=' + encodedLocation;
+}
+  },
+  render: function(){
+    return(
+      <div className = "top-bar">
+        <div className="top-bar-left">
+          <ul className = "menu">
+            <li className = "menu-text">
+              React Timer App
+            </li>
+            <li>
+              <IndexLink to = "/" activeClassName = "active-link" activeStyle = {{fontWeight:'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to = "/" activeClassName = "active-link" activeStyle = {{fontWeight:'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className = "menu">
+          <li className = "menu-text">Created by <a href="http://artemklimenko.com" target ="_blank"> Artem Klimenko</a></li>
+          </ul>
+        </div>
+        </div>
+    );
+  }
+});
+
+
+module.exports = Navigation;
