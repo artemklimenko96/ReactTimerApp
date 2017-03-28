0806bf0368d09eb145d86fb41814ff9f21c1e465
Artem Klimenko <artemklimenko@Artem-MacBook-Pro.local>
2017-03-28 15:03:54 +0300

    added a test for countdown component

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -4,8 +4,34 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var CountDown = require('CountDown');
-describe('CountDown', () =>{
+var Countdown = require('Countdown');
+describe('Countdown', () =>{
   it('should exist', () => {
-    expect(CountDown).toExist();
+    expect(Countdown).toExist();
+  });
+  describe('handleSetCountdown', () =>{
+    it('shoud set state to started and countdown', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() =>{
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001)
+
     });
+    it('should eliminate negative values', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() =>{
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001)
+
+    });
+  });
+});
